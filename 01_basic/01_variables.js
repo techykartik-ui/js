0d8798392e5492e62eb0e80209e09ea3ffc01134
Kartik Vanaliya <kartikvanaliya432@gmail.com>
2026-01-02 12:50:11 +0000

    basic of variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=123
+let accountEmail="kartik@gmail.com"
+var accountPassword="4560"
+accountCity="lathidad"
+let accountState;
+
+// accoutnId = 2 // not allowed
+
+console.log(accountId);
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.table([accountEmail,accountPassword,accountId])
