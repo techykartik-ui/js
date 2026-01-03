5a4ab421e2f407099a5edceab8298475f8f03193
Kartik Vanaliya <kartikvanaliya432@gmail.com>
2026-01-03 09:31:55 +0000

    Conversion of datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,29 @@
+ let score = "Kartik"
+
+ console.log(typeof score);
+ console.log(typeof(score));
+
+ let valueInNumber = Number(score)
+ console.log(typeof valueInNumber);
+ console.log(valueInNumber);
+
+
+ // "33" => 33
+ // "33abc" => Nan
+ // true => 1; false => 0
+
+ let isLoggedIn = "Kartik"
+
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+ console.log(booleanIsLoggedIn);
+
+ // 1=> true; 0=>false
+ // "" => false
+ // "Kartik" => true
+
+ let someNumber = 33
+
+ let stringNumber = String(someNumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
